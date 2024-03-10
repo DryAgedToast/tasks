2874f568e88049ed9ac62b868d4f237e89b19e76
Marc Madlangbayan <MARC21MADLANGBAYAN@GMAIL.COM>
2024-03-10 18:27:16 -0400

    Task 10.1

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,36 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
+import { Button, FormControl, FormGroup, FormLabel } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setattempts] = useState<number>(3);
+    const [ratt, setratt] = useState<string>("");
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>Number of attempts left: {attempts}</div>
+            <FormGroup>
+                <FormLabel>Requested number of additional attempts:</FormLabel>
+                <FormControl
+                    type="number"
+                    value={ratt}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setratt(event.target.value)
+                    }
+                ></FormControl>
+            </FormGroup>
+            <Button
+                onClick={() => setattempts(attempts - 1)}
+                disabled={attempts === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() => setattempts(attempts + parseInt(ratt))}
+                disabled={ratt === ""}
+            >
+                gain
+            </Button>
         </div>
     );
 }
